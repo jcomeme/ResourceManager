d47eed47dcdddca5bb81e701c9db6c4aa6ca5b6c
hara@sunyou.co.jp <hara@ogino-iMac.local>
2011-03-15 17:18:14 +0900

    second commit

--- ResourceManager/ResourceManagerAppDelegate.h
+++ ResourceManager/ResourceManagerAppDelegate.h
@@ -3,7 +3,7 @@
 //  ResourceManager
 //
 //  Created by hara on 11/03/15.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 __Sunyou__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
